f6432f85393d2d8e701b038100cdc4627a234b0c
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-02-19 11:23:09 -0300

    Implementando outras exceções

--- src/main/java/com/ronaldosantos/dscomerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/ronaldosantos/dscomerce/controllers/handlers/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.ronaldosantos.dscomerce.dto.CustomError;
+import com.ronaldosantos.dscomerce.services.exceptions.DatabaseException;
 import com.ronaldosantos.dscomerce.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,8 +18,15 @@ public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
-		
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<CustomError> dataBase(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		
--- src/main/java/com/ronaldosantos/dscomerce/services/ProductService.java
+++ src/main/java/com/ronaldosantos/dscomerce/services/ProductService.java
@@ -1,15 +1,20 @@
 package com.ronaldosantos.dscomerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ronaldosantos.dscomerce.dto.ProductDTO;
 import com.ronaldosantos.dscomerce.entities.Product;
 import com.ronaldosantos.dscomerce.repositories.ProductRepository;
+import com.ronaldosantos.dscomerce.services.exceptions.DatabaseException;
 import com.ronaldosantos.dscomerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
@@ -41,17 +46,30 @@ public class ProductService {
 	
 	@Transactional
 	public ProductDTO update(Long id,ProductDTO dto) {
-		Product entity = repository.getReferenceById(id);
-		copyDtoToEntity(dto,entity);
-		entity = repository.save(entity);
-		return new ProductDTO(entity);
+		try {
+			Product entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto,entity);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
 	}
 	
 	
-	@Transactional
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public void delete(Long id) {
-		repository.deleteById(id);
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
 		}
+		try {
+			repository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Falha de integridade referencial");
+		}
+	}
 
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
