13f8441174386985b5f2776528a0435c7cedec0d
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-02-19 12:36:53 -0300

    Customizando a resposta da validação

---
+++ src/main/java/com/ronaldosantos/dscomerce/dto/ValidationError.java
@@ -0,0 +1,25 @@
+package com.ronaldosantos.dscomerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError {
+	
+	public ValidationError(Instant timestamp, Integer status, String error, String path) {
+		super(timestamp, status, error, path);
+		 
+	}
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+	
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+	
+
+}
